fb05ed8adfdc5605f05db988c20d3aa68a4207c3
Fernando Castelan <fercc1097@gmail.com>
2023-12-14 17:14:28 -0600

    CMVP-194_Login (Email Verify): Adds untracked files

---
+++ src/components/organisms/EmailVerify/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import * as S from './styles'
+import { Text } from '@components/atoms'
+import Image from 'next/image'
+import { colors } from '@/theme'
+
+export const EmailVerify = ({ email, sendEmail }) => {
+  const [didUserResendEmail, setDidUserResendEmail] = useState(false)
+  const handleSendEmail = () => {
+    setDidUserResendEmail(true)
+    sendEmail()
+  }
+  return (
+    <S.MainContainer>
+      <S.SectionContainers>
+        <Image src="icons/emailUnread.svg" height={52} width={52} />
+        <S.MessageContainer>
+          <Text variant="title">Please verify your email</Text>
+          <Text color={colors.grays.info} align="center" variant="info">
+            You're almost there! We sent an email to{' '}
+            {<S.EmailContainer>{email}</S.EmailContainer>}
+          </Text>
+
+          <Text align="center" variant="info">
+            Just click on the link in that email to complete your sign up.
+          </Text>
+        </S.MessageContainer>
+        <S.ResendEmailContainer>
+          <Text color={colors.grays.text} variant="info">
+            Still can't find the email?
+          </Text>
+          {!didUserResendEmail && (
+            <S.ResendEmailButton onClick={handleSendEmail}>
+              <Text color={colors.brand.primary} variant="info">
+                Resend Email
+              </Text>
+            </S.ResendEmailButton>
+          )}
+          {didUserResendEmail && (
+            <Text color={colors.brand.primary} variant="info">
+              Email sent!
+            </Text>
+          )}
+        </S.ResendEmailContainer>
+      </S.SectionContainers>
+    </S.MainContainer>
+  )
+}
