7b0354827b033ac80f77379356cd85998b2d183f
Fernando Castelan <fercc1097@gmail.com>
2023-12-18 12:39:13 -0600

    CMVP-195(Login): Adds dashboard placeholder

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <h1>this is a placeholder for the dashboard.</h1>
+      <p>
+        If the user is not logged in it would be redirected to log in: It'll be
+        done on ticket 191{' '}
+      </p>
+      <p>
+        Maybe you would like to go to <a href="/login">login page</a>
+      </p>
+    </div>
+  )
+}
+
+export default Main
